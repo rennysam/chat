14d6d9be268310f9eb25a921a845c2dab61adb1a
Renny Sam <69663809+rennysam@users.noreply.github.com>
2021-05-08 14:37:36 +0530

    Add files via upload

--- client/main.js
+++ client/main.js
@@ -1,25 +1,25 @@
 var socket = io();
 
-var form = document.querySelector('form');
-
-form.addEventListener('submit', function(e) {
+var form = document.querySelector("form");
+form.addEventListener("submit", function (e) {
   e.preventDefault();
-  var input = document.querySelector('#message');
+  playSound("alert.mp3");
+  var input = document.querySelector("#message");
   var text = input.value;
-  socket.emit('message', text);
-  input.value = '';
+  socket.emit("message", text);
+  input.value = "";
 });
 
-socket.on('message', function(text) {
+socket.on("message", function (text) {
   if (!text) {
     return;
   }
-  var container = document.querySelector('section');
-  var newMessage = document.createElement('p');
+  var container = document.querySelector("section");
+  var newMessage = document.createElement("p");
   newMessage.innerText = text;
   container.appendChild(newMessage);
 
-  var seperator = document.createElement('br');
+  var seperator = document.createElement("br");
   container.appendChild(seperator);
 
   container.scrollTop = container.scrollHeight;
